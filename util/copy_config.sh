654b57cb2e60cd09b4d39aaf692b2f85296f329a
Chri Hamono <40537539+RebelSoftware@users.noreply.github.com>
2023-03-12 19:33:29 +1030

    Create copy_config.sh
    Copies the default config file to the default location if it does not exist

---
+++ util/copy_config.sh
@@ -0,0 +1,17 @@
+CONFIG_PATH="${PWD}/${RECEIPT_PARSER_CONFIG_DIR}/config.yml"
+
+print_fail() {
+    echo -e "\t\033[31m config file is not valid or present\033[0m"
+    echo -e "\t\033[31m Copying default config file to $CONFIG_PATH \033[0m"
+}
+
+# generate api token if non exists
+if [ ! -f "$CONFIG_PATH" ];then
+  print_fail
+  cp "${PWD}/config_default.yml" "$CONFIG_PATH"
+  if [ ! -f "$CONFIG_PATH" ];then
+    echo -e "\t\033[31m config file creation failed \033[0m"
+  fi
+else
+  echo -e "\t\033[32m config file is present \033[0m"
+fi
